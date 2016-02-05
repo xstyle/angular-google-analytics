33925692e1d72d3e8adb209129b2077190c900f7
xstyle <unitxstyle@gmail.com>
2016-02-05 10:13:16 +0300

    parse options after click

--- dist/angular-google-analytics.js
+++ dist/angular-google-analytics.js
@@ -1197,8 +1197,8 @@
       return {
         restrict: 'A',
         link: function (scope, element, attrs) {
-          var options = $parse(attrs.gaTrackEvent);
           element.bind('click', function () {
+            var options = $parse(attrs.gaTrackEvent);
             if(attrs.gaTrackEventIf){
               if(!scope.$eval(attrs.gaTrackEventIf)){
                 return; // Cancel this event if we don't pass the ga-track-event-if condition
